0039ef9f4b8a1030877a71090cafae935733f678
jeanluca554 <jeanluca554@hotmail.com>
2023-02-15 11:34:16 -0300

    refactor: separating into components
    Creating a component for Header, Separator and Sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,52 @@
+import twitterLogo from '../assets/logo-twitter.svg';
+import { Bell, BookmarkSimple, DotsThreeCircle, Envelope, FileText, Hash, House, User } from 'phosphor-react';
+
+import "./Sidebar.css"
+
+export function Sidebar() {
+    return (
+        <aside className='sidebar'>
+            <img className='logo' src={twitterLogo} alt="logo" />
+
+            <nav className='main-navigation'>
+                <a href="" className='active'>
+                    <House weight='fill' />
+                    Home
+                </a>
+                <a href="">
+                    <Hash />
+                    Explore
+                </a>
+                <a href="">
+                    <Bell />
+                    Notifications
+                </a>
+                <a href="">
+                    <Envelope />
+                    Messages
+                </a>
+                <a href="">
+                    <BookmarkSimple />
+                    Bookmarks
+                </a>
+                <a href="">
+                    <FileText />
+                    Lists
+                </a>
+                <a href="">
+                    <User />
+                    Profile
+                </a>
+                <a href="">
+                    <DotsThreeCircle />
+                    More
+                </a>
+            </nav>
+
+            <button className='new-tweet' type="button">
+                Tweet
+            </button>
+        </aside>
+    )
+
+}
